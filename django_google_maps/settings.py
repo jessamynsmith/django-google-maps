ab6eb2cfb738019c9f4e8856c290d2dbfae18f72
jessamynsmith <jessamyn.smith@gmail.com>
2017-10-25 21:49:18 -0600

    Make API available only to authenticated users.

--- django_google_maps/settings.py
+++ django_google_maps/settings.py
@@ -111,6 +111,17 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
