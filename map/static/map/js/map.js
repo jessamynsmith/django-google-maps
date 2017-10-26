d2a78a3a5095d694f42cbfd82452c70bd661b4df
jessamynsmith <jessamyn.smith@gmail.com>
2017-10-25 22:41:54 -0600

    Added search functionality.

--- map/static/map/js/map.js
+++ map/static/map/js/map.js
@@ -1,6 +1,12 @@
 "use strict";
 
 
+var position = null;
+var map = null;
+var infowindow = null;
+var markers = [];
+
+
 // Default to downtown Toronto
 var defaultPosition = {
   coords: {
@@ -9,19 +15,68 @@ var defaultPosition = {
   }
 };
 
-var populateMap = function (position) {
+
+var yelpSearch = function(map, infoWindow, position, term) {
+
+  var ajaxData = {
+    limit: 10,
+    radius: 200,
+    sort: 'distance',
+    latitude: position.coords.latitude,
+    longitude: position.coords.longitude
+  };
+  if (term) {
+    ajaxData.term = term;
+  }
+
+  $.ajax({
+    method: "get",
+    url: '/api/v1/yelp/search/',
+    data: ajaxData
+  }).done(function (data) {
+    for (var i = 0; i < data.businesses.length; i++) {
+      var business = data.businesses[i];
+      var marker = new google.maps.Marker({
+        id: i,
+        title: business.name,
+        url: business.url,
+        position: {
+          lat: business.coordinates.latitude,
+          lng: business.coordinates.longitude
+        },
+        map: map
+      });
+      markers.push(marker);
+
+      google.maps.event.addListener(marker, 'click', (function (marker, key) {
+        return function () {
+          var content = '<div><a target="_blank" href="' + marker.url + '">' + marker.title + '</a></div>';
+          infowindow.setContent(content);
+          infowindow.open(map, marker);
+        };
+      })(marker, i));
+    }
+  }).fail(function (error) {
+    console.log("Unable to access yelp");
+    console.log(error);
+  });
+};
+
+
+var populateMap = function (geoPosition, term) {
 
   $('#geolocation').hide();
 
-  var infowindow = new google.maps.InfoWindow({
+  infowindow = new google.maps.InfoWindow({
     content: "hello"
   });
 
+  position = geoPosition;
   if (!position) {
     position = defaultPosition;
   }
 
-  var map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     center: {
       lat: position.coords.latitude,
       lng: position.coords.longitude
@@ -37,42 +92,7 @@ var populateMap = function (position) {
     }
   };
 
-  $.ajax({
-    method: "get",
-    url: '/api/v1/yelp/search/',
-    data: {
-      limit: 10,
-      radius: 200,
-      sort: 'distance',
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude
-    }
-  }).done(function (data) {
-    for (var i = 0; i < data.businesses.length; i++) {
-      var business = data.businesses[i];
-      var marker = new google.maps.Marker({
-        id: i,
-        title: business.name,
-        url: business.url,
-        position: {
-          lat: business.coordinates.latitude,
-          lng: business.coordinates.longitude
-        },
-        map: map
-      });
-
-      google.maps.event.addListener(marker, 'click', (function (marker, key) {
-        return function () {
-          var content = '<div><a target="_blank" href="' + marker.url + '">' + marker.title + '</a></div>';
-          infowindow.setContent(content);
-          infowindow.open(map, marker);
-        };
-      })(marker, i));
-    }
-  }).fail(function (error) {
-    console.log("Unable to access yelp");
-    console.log(error);
-  });
+  yelpSearch(map, infowindow, position);
 
 };
 
@@ -98,3 +118,19 @@ var initMap = function() {
     geolocationFail();
   }
 };
+
+
+var clearMarkers = function(markers) {
+  for (var i=0; i<markers.length; i++) {
+    markers[i].setMap(null);
+  }
+  markers = [];
+};
+
+
+$('#id_search_form').on('submit', function(event) {
+  event.preventDefault();
+  clearMarkers(markers);
+  yelpSearch(map, infowindow, position, $('#id_term').val());
+});
+
