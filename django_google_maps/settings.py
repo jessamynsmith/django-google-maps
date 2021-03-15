51ce78f09d8fd870500cc496436c4cad9f350334
Jessamyn Smith <geekchick77@gmail.com>
2021-03-15 12:12:46 -0600

    Upgrade djangorestframework version.

--- django_google_maps/settings.py
+++ django_google_maps/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
 # Use env setting if available, otherwise make debug false
 DEBUG = bool(int(os.environ.get('DJANGO_DEBUG', '1')))
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'django-google-maps.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'django-google-maps.herokuapp.com']
 
 SECURE_SSL_REDIRECT = bool(int(os.environ.get('DJANGO_ENABLE_SSL', '0')))
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
